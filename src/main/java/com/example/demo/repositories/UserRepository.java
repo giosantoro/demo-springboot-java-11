8340607cb0f3e32b2b1e693ab75405b933e4997f
Giovanni Santoro <giggio.santoro@gmail.com>
2021-04-11 13:06:12 -0300

    Service layer, component registration

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entities.User;
 
 //this is the way to access many repository functions
+//you don't need to put the @Repository register as it extends the jparepository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,28 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+//this item must be registered as a component for the dependency injection works (autowired)
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.get();
+	}
+
+}
