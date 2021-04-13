58403bff6bacc56d434b67502614c10c41a86715
Giovanni Santoro <giggio.santoro@gmail.com>
2021-04-13 19:29:00 -0300

    Payment one-to-one association

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.example.demo.entities.enums.OrderStatus;
@@ -41,6 +43,10 @@ public class Order implements Serializable{
 	//it has to access the orderItem and only then find the order by the id
 	@OneToMany(mappedBy="id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	//the entities must have the same id, that's why the cascade
+	@OneToOne(mappedBy="order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -86,6 +92,14 @@ public class Order implements Serializable{
 		this.client = client;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems(){
 		return items;
 	}
